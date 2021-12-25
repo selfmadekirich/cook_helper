f5900a18cb0a4b41f6b6f55244231c38336da59d
selfmadekirich <tosha.kirichenko.2001@mail.ru>
2021-12-26 00:07:02 +0300

    add validation for registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,22 @@
 class UsersController < ApplicationController
   def new
     @user = User.new
+
   end
 
   def create
+    id = User.find_by login: user_params[:login], username: user_params[:username]
+
+    if not id.nil?
+      flash[:errors] = 'login or username has already been used'
+      redirect_to :action => :new  and return
+    end
     @user = User.create(user_params)
     if @user.valid?
-      redirect_to
+      redirect_to :controller => :measures
     else
-      puts @user.errors.full_messages.join
-      redirect_to :new , alert: @user.errors.full_messages.join
+      flash[:errors] =  @user.errors.full_messages.join
+      redirect_to :action => :new
     end
 
     end
